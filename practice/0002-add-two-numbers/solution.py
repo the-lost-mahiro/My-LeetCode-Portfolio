7d51286c066e76a460386e98263abae98a53d2ca
Mahirou <ngyuenvanan6a5@gmail.com>
2025-12-15 16:08:33 +0000

    Sync LeetCode submission Runtime - 4 ms (54.65%), Memory - 17.9 MB (56.28%)

--- practice/0002-add-two-numbers/solution.py
+++ practice/0002-add-two-numbers/solution.py
@@ -5,34 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        a = []
-        current = l1
-        while current:
-            a.append(current.val)
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            total = val1 + val2 + carry
+            carry = total // 10
+            new_digit = total % 10  
+
+            current.next = ListNode(new_digit)
             current = current.next
-        current = l2
 
-        b=[]
-        while current:
-            b.append(current.val)
-            current = current.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
 
-        l = max(len(a), len(b))
-        while len(a) < l:
-            a.append(0)
-        while len(b) < l:
-            b.append(0)
-        
-        out = 0
-        ans = []
-        for i in range(l):
-            ans.append(ListNode((a[i] + b[i] + out)%10))
-            out = (a[i] + b[i] + out)//10
-
-        if out != 0:
-            ans.append(ListNode(out))
-
-        for i in range(len(ans)-1):
-            ans[i].next = ans[i+1]
-        
-        return ans[0]
+        return dummy.next
