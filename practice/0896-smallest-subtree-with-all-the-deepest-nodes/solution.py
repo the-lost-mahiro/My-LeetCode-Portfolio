51d141356ffa4a75581f7c764c1dd9c24ed2145b
Mahirou <ngyuenvanan6a5@gmail.com>
2026-01-09 11:05:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (95.32%)

---
+++ practice/0896-smallest-subtree-with-all-the-deepest-nodes/solution.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        def dfs(node):
+            if not node:
+                return None, 0
+            
+            left_node, left_depth = dfs(node.left)
+            right_node, right_depth = dfs(node.right)
+            
+            if left_depth == right_depth:
+                return node, left_depth + 1
+            
+            elif left_depth > right_depth:
+                return left_node, left_depth + 1
+            
+            else:
+                return right_node, right_depth + 1
+        
+        result_node, max_depth = dfs(root)
+        return result_node
